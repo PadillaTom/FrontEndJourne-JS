81b31c825b7c56d915ff8d6bb514be96af40f574
Tomas Padilla <Padillatomasagustin@gmail.com>
2020-07-30 16:38:48 +0200

    Comenzamos Variables

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 //  ::::::::::::::
+
 //  :::::::::::::: Helper Methods
 // document.write("Hello World");
 // alert("Hello People");
 // console.log("Hello Guys");
 
 //  :::::::::::::: Statements and Comments
+// console.log('hello wolrd');
+// console.log('hello people');
+// console.log('hello wolrd');
+// console.log('hello wolrd');
+
+//  :::::::::::::: Variables
