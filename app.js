354d6cad04bfffb76dca4e64b35edcd91a255494
Tomas Padilla <Padillatomasagustin@gmail.com>
2020-07-30 16:14:05 +0200

    Comenzamos JS, statements and comments

---
+++ app.js
@@ -0,0 +1,7 @@
+//  ::::::::::::::
+//  :::::::::::::: Helper Methods
+// document.write("Hello World");
+// alert("Hello People");
+// console.log("Hello Guys");
+
+//  :::::::::::::: Statements and Comments
