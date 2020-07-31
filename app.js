17b9674cdec83b8ed18213e8f541ec2dbd80924f
Tomas Padilla <Padillatomasagustin@gmail.com>
2020-07-31 16:36:18 +0200

    Const letvar

--- app.js
+++ app.js
@@ -12,3 +12,21 @@
 // console.log('hello wolrd');
 
 //  :::::::::::::: Variables
+let name = 'Tom';
+console.log('Hello', name);
+
+let address, zip, state;
+address = 'France';
+zip = '74160';
+state = 'Collonges';
+console.log(
+  'Hello, my name is',
+  name,
+  'and I live in',
+  address,
+  'with the zip:',
+  zip,
+  'in',
+  state,
+  '.'
+);
