aa588c13e516bf2c7e1adc6dcea8418b754900a2
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-10 09:22:00 +0200

    quotation marks

--- app.js
+++ app.js
@@ -12,21 +12,30 @@
 // console.log('hello wolrd');
 
 //  :::::::::::::: Variables
-let name = 'Tom';
-console.log('Hello', name);
+// let name = 'Tom';
+// console.log('Hello', name);
 
-let address, zip, state;
-address = 'France';
-zip = '74160';
-state = 'Collonges';
-console.log(
-  'Hello, my name is',
-  name,
-  'and I live in',
-  address,
-  'with the zip:',
-  zip,
-  'in',
-  state,
-  '.'
-);
+// let address, zip, state;
+// address = 'France';
+// zip = '74160';
+// state = 'Collonges';
+// console.log(
+//   'Hello, my name is',
+//   name,
+//   'and I live in',
+//   address,
+//   'with the zip:',
+//   zip,
+//   'in',
+//   state,
+//   '.'
+// );
+
+//  :::::::::::::: Const, Let, Var
+// Var was replaced by Let and Const. NO SE USA MAS!!!!
+// Const --> Cannot be reasigned, cannot be empty, cannot change values or it gives an error.
+// Let --> Recomendable usarlo para todo.
+// Var --> Solo se usa en E6.
+// La mayoria de programadores usa Const y luego modifica valores. Muy pocas veces usan Let.
+//
+//
