437826d91cf7283cc566c8feb6d05d1ec91b708e
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-11 11:50:14 +0200

    basi JS tutorial part 1

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
 //  :::::::::::::: Variables
 // let name = 'Tom';
 // console.log('Hello', name);
-
+//
 // let address, zip, state;
 // address = 'France';
 // zip = '74160';
@@ -39,7 +39,147 @@
 // La mayoria de programadores usa Const y luego modifica valores. Muy pocas veces usan Let.
 
 //  :::::::::::::: Quotation Marks "" or ''
-// Continuar Eloquen tambien
+// There is NO DIFFERENCE
+// To have them as a string we need to escape them by adding a \ before them
+// const name ="John\'s has a blue car"
+
+//  :::::::::::::: String Concatenation
+// Combining Strings Values
+//
+// const name = 'john';
+// const lastName = 'padilla';
+// let fullname = name + ' ' + lastName;
+// console.log('Hello i am ' + name + ' .');
+// console.log(' i am ' + fullname);
+//
+// const website = 'google';
+// const url = 'www.' + website + '.com';
+// console.log(url);
+
+//  :::::::::::::: Numbers
+// "Loosely Typed" = we don't have to specify which value we add
+// Strings are BLACK, numbers are BLUE
+// const number1 = 100;
+// let pants = 2.456;
+// pants = 'are great';
+// console.log(number1);
+// console.log(pants);
+// const number2 = 25;
+// console.log(number2);
+// If we see a BLACK NUMBER!!! pay attention it is a STRING!!
+// const add = number1 + number2;
+// const mult = number1 * number2;
+// const resta = number1 - number2;
+// const div = number1 / number2;
+// console.log(add);
+// console.log(mult);
+// console.log(resta);
+// console.log(div);
+
+//  :::::::::::::: Operators
+// +=, -=, *=, /=, ++, --, %
+// let number = 50;
+// number += 5;  -----> Suma 5
+// number += 5;
+// number += 5;
+// console.log(number);
+// number++;     -----> Suma 1
+// number++;
+// console.log(number);
+//
+// Modulus %  --> SOBRANTE // REMAINDER
+// Cuanto es lo que SOBRA de una division
+// const pizzaSlices = 10;
+// const children = 3;
+// const sobran = pizzaSlices % children;
+// console.log(sobran);
+
+//  :::::::::::::: Type Conversion
+//
+//
+//
+//
+
+//  :::::::::::::: Data Types
+//
+//
+//
+//
+
+//  :::::::::::::: Arrays
+//
+//
+//
+//
+
+//  :::::::::::::: Functions
+//
+//
+//
+//
+
+//  :::::::::::::: Funct: Parameters and Arguments
+//
+//
+//
+//
+
+//  :::::::::::::: Funct: Return
+//
+//
+//
+//
+
+//  :::::::::::::: Funct: Expressions
+//
+//
+//
+//
+
+//  :::::::::::::: Objects
+//
+//
+//
+//
+
+//  :::::::::::::: Conditional Statements
+//
+//
+//
+//
+
+//  :::::::::::::: Equality
+//
+//
+//
+//
+
+//  :::::::::::::: Logial Operators
+//
+//
+//
+//
+
+//  :::::::::::::: Switch Statement
+//
+//
+//
+//
+
+//  :::::::::::::: Loops: WHILE
+//
+//
+//
+//
+
+//  :::::::::::::: Loops: DO WHILE
+//
+//
+//
+//
+
+//  :::::::::::::: Loops: FOR
+//
 //
 //
 //
