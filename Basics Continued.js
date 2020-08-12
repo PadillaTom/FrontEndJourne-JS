f7f8b12e59ef4dbaf5f110d9151a6831b077ec82
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-12 12:25:45 +0200

    Template Literals

---
+++ Basics Continued.js
@@ -0,0 +1,157 @@
+//  :::::::::::::: String Properties and Methods
+// Don't learn all methods. Search them in Google
+// let text = 'Tomas Padilla';
+
+// Prop-- LENGTH: Cantidad de Caracteres.
+// let resultLength = text.length;
+// console.log(resultLength);
+
+//Met-- LOWER and UPPER case.
+// console.log(text.toLowerCase());
+// console.log(text.toUpperCase());
+
+// Met-- Show Character in Index
+// console.log(text.charAt(4));
+// console.log(text.charAt(12));
+// Last Character
+// console.log(text.charAt(text.length - 1));
+// From index show character:
+//If i don't have it in my string: -1;
+// If there are lots: Returns the first one.
+// console.log(text.indexOf('a'));
+
+//TRIM: Si tengo un espacio al principio, lo acorta hasta la primer letra.
+// console.log(text);
+// console.log(text.trim());
+// Ej: tenemos " Tomas Padilla" --> Con espacio antes
+// let text2 = ' Tomas Padilla';
+// console.log(text2.startsWith('Tomas'));
+// Nos tira FALSE : PORQUE NO PUSIMOS EL ESPACIO ANTES DE TOMAS
+// Solucion:
+// console.log(text.trim().toLocaleLowerCase().startsWith('tomas'));
+// TRUE!!!!!!!!!
+// INCLUDES: Por supuesto podemos aplicar la misma solucion que antes
+// console.log(text.includes('oma'));
+// SLICE: From , To:
+// console.log(text2.slice(1, 4)); // From 1, to 4.
+// console.log(text2.slice(-7)); //From last -7.
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
+
+//  ::::::::::::::
+//
+//
+//
+//
