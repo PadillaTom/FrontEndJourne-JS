9c5f2bd05ecf5461b0dcf33b00eb6c753eff90d7
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-13 10:22:30 +0200

    Value Reference

--- Basics Continued.js
+++ Basics Continued.js
@@ -1,26 +1,23 @@
 //  :::::::::::::: String Properties and Methods
 // Don't learn all methods. Search them in Google
 // let text = 'Tomas Padilla';
-
-// Prop-- LENGTH: Cantidad de Caracteres.
+// -----> LENGTH <-----
+// Cantidad de Caracteres.
 // let resultLength = text.length;
 // console.log(resultLength);
-
-//Met-- LOWER and UPPER case.
+//-----> LOWER and UPPER case. <-----
 // console.log(text.toLowerCase());
 // console.log(text.toUpperCase());
-
-// Met-- Show Character in Index
+// -----> CHARAT <-----
 // console.log(text.charAt(4));
 // console.log(text.charAt(12));
-// Last Character
 // console.log(text.charAt(text.length - 1));
-// From index show character:
+// -----> INDEXOF <-----
 //If i don't have it in my string: -1;
 // If there are lots: Returns the first one.
 // console.log(text.indexOf('a'));
-
-//TRIM: Si tengo un espacio al principio, lo acorta hasta la primer letra.
+// -----> TRIM <-----
+//Si tengo un espacio al principio, lo acorta hasta la primer letra.
 // console.log(text);
 // console.log(text.trim());
 // Ej: tenemos " Tomas Padilla" --> Con espacio antes
@@ -30,23 +27,57 @@
 // Solucion:
 // console.log(text.trim().toLocaleLowerCase().startsWith('tomas'));
 // TRUE!!!!!!!!!
-// INCLUDES: Por supuesto podemos aplicar la misma solucion que antes
+// -----> INCLUDES <-----
+// Por supuesto podemos aplicar la misma solucion que antes
 // console.log(text.includes('oma'));
-// SLICE: From , To:
+// -----> SLICE <-----
+// From , To:
 // console.log(text2.slice(1, 4)); // From 1, to 4.
 // console.log(text2.slice(-7)); //From last -7.
 
-//  ::::::::::::::
-//
-//
-//
-//
+//  :::::::::::::: Template Literals
+// We use BACKTICK Characters
+// Perform Operations: ${}
+// No need to escape
+// USADO EN EL DOM PARA INSERTAR ELEMENTOS
+// let name = 'tom';
+// const string = `hello i'm *\ asd ${name} ${5 * 5} `;
+// console.log(string);
 
-//  ::::::::::::::
-//
-//
-//
-//
+//  :::::::::::::: Arrays Prop and Methods
+// let namesList = ['tom', 'katy', 'july'];
+// // -----> LENGTH <-----
+// console.log(namesList.length);
+// console.log(namesList[namesList.length - 1]);
+// console.log(namesList[1]);
+// //  -----> CONTACT<-----
+// let lastList = ['pepe', 'padi', 'torres'];
+// const allNames = namesList.concat(lastList);
+// console.log(allNames);
+// //-----> REVERSE <-----
+// console.log(allNames.reverse());
+// // -----> SHIFT / UNSHIFT <-----
+// // Unshift :Agrega al comienzo de la lista.
+// allNames.unshift('katy2');
+// allNames.unshift('padi2');
+// console.log(allNames);
+// //Shift: Remueve el priemro de la lista
+// allNames.shift();
+// allNames.shift();
+// console.log(allNames);
+// // -----> PUSH / POP <-----
+// // Pop: Remueve el final
+// allNames.pop();
+// console.log(allNames);
+// // Push: Agrega al final de la lista
+// allNames.push('tom2');
+// console.log(allNames);
+// // -----> SPLICE <-----
+// // MODIFICA REALMENTE LA ARRAY!!!! (mutate)
+// // splice (index de comienzo, Cuantos remueve)
+// let specificNames = allNames.splice(-1, 1); // A partir del ultimo index, tomar 1 solo item.
+// console.log(specificNames);
+// console.log(allNames);
 
 //  ::::::::::::::
 //
---
+++ Excercices.js
@@ -0,0 +1,54 @@
+// Ejercicios de Udemy (John Smilga)
+
+// Tutorial Continued
+
+// :::::::: FULL NAME ::::::::::
+// const names = ['anna', 'susy', 'bob', 'john'];
+// const lastName = ['shakeandbake'];
+// let newArray = [];
+// //for loop : Itera todos los items.
+// for (let i = 0; i < names.length; i++) {
+//   console.log(i); // Su numero index
+//   console.log(names[i]); // Su value
+//   newArray.push(names[i]); // Add names to newarray
+//   //   newArray.push(`${names[i]} ${lastName}`); // Add lastname to name inside newarray
+//   const fullName = [];
+//   fullName.push(`${names[i]} ${lastName}`);
+//   console.log(fullName);
+// }
+// console.log(newArray);
+
+// :::::::: CALCULATE TOTAL ::::::::::
+// const gas = [20, 40, 100];
+// const food = [10, 40, 50];
+
+// function calculateTotals(arr) {
+//   let total = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     // console.log(arr[i]); // Consejo: Instant feedback; pasar un log para ver que todo funciona bien
+//     total += arr[i]; // Suma cada item del parametro y lo aloja en TOTAL
+//   }
+//   if (total > 100) {
+//     console.log('Atencion wacho!');
+//     return total;
+//   }
+//   console.log('Vas bien, gastas menos de 100');
+//   //   console.log(total); // probamos
+//   return total;
+// }
+// // calculateTotals(gas); // probamos;
+// const gasTotal = calculateTotals(gas);
+// const foodTotal = calculateTotals(food);
+// const randomTotal = calculateTotals([150, 50, 200, 100]);
+// // multiple logs: Creamos un object desde LOG.
+// console.log({
+//   gas: gasTotal,
+//   good: foodTotal,
+//   random: randomTotal,
+// });
+
+// :::::::: Full Name ::::::::::
+
+// :::::::: Full Name ::::::::::
+
+// :::::::: Full Name ::::::::::
