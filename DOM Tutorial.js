1899212f192b87a8a6b49dafc91cd02e76b4605b
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-14 09:39:50 +0200

    Comenzamos DOM hoy viernes

---
+++ DOM Tutorial.js
@@ -0,0 +1,119 @@
+// DOM:
+// Document Object Model
+// When we have an HTML, the browser turns it into a DOM (each element becomes an object)
+// and we can use JS to interact with these elements.
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
+
+// :::::  General Concepts   :::::
+//
+//
+//
