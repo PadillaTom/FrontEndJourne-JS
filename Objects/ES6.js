eff189a5bb0fd5994d934ef03ae90ab5132cf3c3
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-22 13:09:49 +0200

    Button ex

--- Objects/ES6.js
+++ Objects/ES6.js
@@ -1,25 +1,77 @@
-// Es6 Syntactic Sugar
-// Usando clases la sntax es mas limpia
-class Account {
-  constructor(name, initialBalance) {
-    // SPECIAL KEYWORD
-    this.name = name;
-    this.balance = initialBalance;
-  }
-  bank = 'Padis Banks';
-  deposit(amount) {
-    this.balance += amount;
-    console.log(`Your New Balance is ${this.balance}`);
-  }
-}
+// ::::::: Es6 Syntactic Sugar :::::::
+//
+// Usando clases la syntax es mas limpia
+// class Account { // Class Mayus
+//   constructor(name, initialBalance) {
+//     // SPECIAL KEYWORD that will run everything
+//     this.name = name;
+//     this.balance = initialBalance;
+//   }
+//   bank = 'Padis Banks'; // This is like a Prototype in ES6
+//   deposit(amount) { // A function
+//     this.balance += amount;
+//     console.log(`Your New Balance is ${this.balance}`);
+//   }
+// }
 
-const tom = new Account('tom', 0);
-console.log(tom); // Vemos que dentro de la CLASS ACCOUNT hay un constructor
-console.log(tom.name); // Accedemos a valores
+// const tom = new Account('tom', 0);
+// console.log(tom); // Vemos que dentro de la CLASS ACCOUNT hay un constructor
+// console.log(tom.name); // Accedemos a valores
 // Para pasar valores dentro de la instance se usa CONSTRUCTOR(){}
-tom.deposit(1000); // Accedemos a deposit
+// tom.deposit(1000); // Accedemos a deposit
 //
 // Si queremos algo como PROTOTYPE se usará:
 // bank = "Padis Bank" ---> SIN THIS
 //
 //
+// :::::: Call  ||  Apply ||  Bind ::::::
+// CALL ----->
+const john = {
+  name: 'John',
+  age: 25,
+  greet: function () {
+    console.log(this); // Mostramos a quien refiere THIS
+    console.log(`Hello i am ${this.name} and i'am ${this.age}`);
+  },
+};
+const bob = {
+  name: 'Bobby',
+  age: 40,
+};
+// john.greet();
+//
+// Entonces si quremos la misma Function (greet) para 50k personas.... deberíamos copiar y pegar.....
+// Solucion: usamos el CALL METHOD
+// Call--> Runs instantly
+// FUNCTION FROM OUTSIDE-->
+function greet(city) {
+  console.log(this);
+  console.log(`Hi i am ${this.name} and i am ${this.age}. I live in ${city}`);
+}
+// greet.call(john);
+// greet.call(bob);
+// Pasamos los objects creados... si no hay creados?
+// greet.call({ name: 'Tommy', age: 25 });
+// FUNCTION AS METHOD:
+// john.greet.call(bob); // Imprime BOB, agarrando el Method dentro de Tom
+// Arguments in Function:
+greet.call(john, 'Capital');
+greet.call(bob, 'Olivos');
+greet.call({ name: 'Tommy', age: 25 }, 'Arg');
+//
+//
+// APPLY ----->
+// Retocamos la FUNC GREET (city)
+// Igual que Call, pero por un ARRAY
+greet.apply(john, ['Collonges']);
+greet.apply(bob, ['haute']);
+greet.apply({ name: 'susan', age: 25 }, ['America']);
+// Queremos meter una City
+//
+//
+// BIND ------>
+// Lo podemos asignar y usar mas tarde, Pasa LIST (como call)
+// 1) Assign the greet
+const susanGreet = greet.bind({ name: 'Susy', age: 25 }, 'France');
+// 2) Invoke the greet.
+susanGreet();
