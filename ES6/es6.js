61af8b20c3b890de1d48a4aa299cdc3bd5a26ec7
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-25 14:34:31 +0200

    Comenzamos ES6

---
+++ ES6/es6.js
@@ -0,0 +1,6 @@
+// :::::::::: ES6 :::::::::::::
+// Bron in 1995, it needs to keep up with time.
+// New Versions. In 2015 = ES6.
+// There is es7, es8. But ES6 is the LATEST MAJOR UPDATE.
+// Here we will see as well es7, and es8.
+//
