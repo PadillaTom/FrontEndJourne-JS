49b646990d23c397d7206eb981da8b0939c598a3
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-25 09:37:50 +0200

    modal setup

--- Objects/OOP Projects/Gallery/app.js
+++ Objects/OOP Projects/Gallery/app.js
@@ -1,7 +1,7 @@
 // ::::: Gallery Proto ::::::::
 
-// :: Selection Function ::
-//
+// ::: Functions :::
+// We will select elements by: getElement (".class")
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
@@ -12,5 +12,24 @@ function getElement(selection) {
   );
 }
 //
+// :::::::: CONSTRUCTIVE FUNCTION :::::::
+// We want to pass in the section of Nature or City, to access all images inside each section.
+function Gallery(element) {
+  // console.log(element); // Una vez done the selections: Test to see.
+  // --> Select all Images in an Array:
+  this.list = [...element.querySelectorAll('.img')]; // Necesitamos pasar NodeList to Array --> [...]
+  // console.log(this.list); // Vemos que es una Node List... Nos conviene pasar a Array (usamos ES6)
+  // --> Select Modal and everything inside it:
+  this.modal = getElement('.modal'); // Para dar OPEN
+  this.modalImg = getElement('.main-img'); // Para mostrarla en grande
+  this.modalImages = getElement('.modal-images'); // Para mostrar debajo
+  this.closeBtn = getElement('.close-btn');
+  this.nextBtn = getElement('.next-btn');
+  this.prevBtn = getElement('.prev-btn');
+}
+//
+// ::: Selections :::
+const nature = new Gallery(getElement('.nature'));
+const city = new Gallery(getElement('.city'));
 //
 //
