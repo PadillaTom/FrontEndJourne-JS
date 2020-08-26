3405b28d41e667ba9e424d0ce6c37c0b2d7bc8eb
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-26 08:31:49 +0200

    Array Destructuring

--- ES6/es6.js
+++ ES6/es6.js
@@ -4,7 +4,7 @@
 // There is es7, es8. But ES6 is the LATEST MAJOR UPDATE.
 // Here we will see as well es7, and es8.
 
-// --------------------------> VAR, LET, CONST
+// --------------------------> VAR, LET, CONST <----------------------------
 // Define, Update, Redefine by using VAR !!! ----> NOT GOOD
 // CONST cannot mutate primitive types.
 //
@@ -52,14 +52,14 @@
 // console.log(total);
 //
 
-// --------------------------> Template Strings
+// --------------------------> TEMPLATE STRINGS <----------------------------
 // `` --> Podemos pasar cualquier tipo de " " '' y variables.
 // Ademas: ${} nos permite acceder a values!
 // const name = 'TOM';
 // console.log(`my name is : ${name}`);
 //
 
-// --------------------------> Template String HTML
+// --------------------------> TEMPLATE STRINGS IN HTML <----------------------------
 // Lo mejor de Template Literals es poder usarla para HTML dynamicamente.
 // result.innerHTML = `<p class="tu vieja"> ${num1+num2} </p>`
 // Otro Ejemplo mas complejo:
@@ -81,7 +81,7 @@
 // `; // MAP separa los elementos con Commas ",". usando JOIN (" ") Reemplazamos las Commas por espacios
 //
 
-// --------------------------> Tagged Template Literals
+// --------------------------> TAGGED TEMPLATE LITERALS <----------------------------
 // Poder pasar las Template Literals por una Function. Components Libraries are built using Template Literals
 // const author = 'Some Author';
 // const statement = 'Some Statement';
@@ -109,55 +109,180 @@
 //"texto[0] + strong + var[0] + texto[1] + var[1]" ---> SE REPITE
 // || " " -----> QUIERE DECIR QUE AL FINAL IMPRIME ESPACIO, en vez del ultimo item del Array Text (que daría UNDEFINED)
 
-// --------------------------> Arrow Function Basics
+// --------------------------> ARROW FUNCTIONS BASICS <----------------------------
+// Regular Function : function sayHello () {} --> sayHello();
+// Function Expressions : const hello = function (name){log name} --> hello("tom")
+//
+// With ES6 we have "Fat Arrow Functions" (arrow functions)
+// No Name (always as expressions: Assigned to a variable)
+// No Function keyword
+// They treat THIS differently
+//
+// Tenemos un BTN en HTML:
+// 1) One-Liner: Algo que se ejecuta de toke.
+// const sayHello = () => console.log('Hello');
+// sayHello();
+//
+// 2) Parameters: Pasamos un Value asignado a otra variable
+// const double = (value) => value * 2;
+// const num = double(4);
+// console.log(num);
+//
+// 3) 2 Parameters and more than one line:
+// const multiply = (num1, num2) => {
+//   const result = num1 * num2;
+// More code
+//   return result;
+// };
+// const sum = multiply(5, 5);
+// console.log(sum);
+//
+// 4) Return an Object:
+// const object = () => {name:"Tom", age: 25}; // ERROR porque no distingue object de multiline
+// Solucion: Ponemos el Object entre parentesis
+// const object = () => ({ name: 'Tom', age: 25 }); // Ahora Podemos utilizarlo
+// const person = object();
+// console.log(person);
+//
+// 5) As Callback Functions:
+// Ejemplo --> Filter numbers bigger than 2.
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const big = numbers.filter((number) => number > 2); // (number) "arrow: return if" (condition num >2)
+// console.log(big); // Devuelve un Array
+// Ejemplo 2 --> Multiple values:
+// const big2 = numbers.filter((number, index) => {
+//   console.log(index); // Devuelve el Index de los elementos del Array
+//   return number > 2; // Mayores de 2
+// });
+// console.log(big2); // Invokamos
+//
+// 6) As EventListener: Tenemos un BTN que LOG cuando clicked.
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', () => console.log('You Clicked Me!'));
+
+// Objects and THIS
+// This Regular ---> This hace referencia al PARENT una vez asignado dentro de la Function.
+// This ARROW ----> Hace referencia al SURROUND de donde fue Assignado.!!!
+// Ejemplo: Dos Objects Bob(Regular) and Anna(Arrow)
+
+// const bob = {
+//   firstName: 'Bob',
+//   lastName: 'Smith',
+//   sayName: function () {
+//     console.log(this); // Mostramos a donde apunta THIS --> Bob
+//     const self = this;
+//     setTimeout(function () {
+//       //   console.log(this); // Apunta a Window Porque BOB ya fue ejecutado
+//       console.log(self); // Apunta a BOB, donde fue asignado SELF
+//       console.log(`Hi my name is : ${self.firstName} ${self.lastName}`);
+//     }, 2000);
+//   },
+// };
+// const anna = {
+//   firstName: 'Anna',
+//   lastName: 'Sanders',
+//   sayName: () => {
+//     console.log(this); // Mostramos a donde apunta THIS --> Window
+//     console.log(`Hi my name is : ${this.firstName} ${this.lastName}`);
+//   },
+// };
+// bob.sayName();
+// anna.sayName();
+//
+// AHORA: Usamos TimeOut de 2s para BOB: Ver arriba.
+// Invoka Bob - 2s after - Invoka la Timeout ... THIS apunta a window ya que bob fue invokado previamente!
+// Solucion: const self = this --> Dentro de BOB
+// Solucion 2: Usamos una Arrow Function donde THIS referencia a su SURROUND, no a su assignacion, y permite acceder a BOB.
+// Ver Bob2 -->
+// const bob2 = {
+//   firstName: 'Bob 2',
+//   lastName: 'Smith 2',
+//   sayName: function () {
+//     console.log(this); // Mostramos a donde apunta THIS --> Bob
+//     setTimeout(() => {
+//       console.log(this); // Apunta al OBJECT bob2, ya que hace referencia al SURROUND, NO PARENT
+//       console.log(`Hi my name is : ${this.firstName} ${this.lastName}`);
+//     }, 2000);
+//   },
+// };
+// bob2.sayName();
+//
+// Selecting Elements and THIS:
+// Regular Function:
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', function () {
+//   console.log(this); // This hace referencia al Button
+//   this.style.color = 'blue';
+// });
+// Arrow Function:
+// btn = document.querySelector('.btn');
+// btn.addEventListener('click', () => {
+//   console.log(this); // This hace referencia al WINDOW
+//   this.style.color = 'blue';  // TIRA ERROR!
+// });
+// HORA: Queremos TimeOut 2s:
+// btn = document.querySelector('.btn');
+// btn.addEventListener('click', function () {
+//   console.log(this); // Button
+//   setTimeout(function () {
+//     console.log(this);
+//     this.style.color = 'blue'; // Luego de 2s, btn ya fue clickeado... This refers to Window!!!
+//   }, 2000);
+// });
+// Solucion: Arrow Functions ;)
+// btn = document.querySelector('.btn');
+// btn.addEventListener('click', function () {
+//   console.log(this); // Button
+//   setTimeout(() => {
+//     console.log(this); // This apunta a Button ( Surrounds the ARROW!)
+//     this.style.color = 'blue'; // This apunta a Button! (ya que es su SURROUNDING!)
+//   }, 2000);
+// });
+
+// Default Parameters and Arrow Functions "gotchas"
+// const john = 'John';
+// const peter = 'Peter';
+// // Regular
+// function sayHi(person = 'Default') {
+//   console.log(`Regular says Hi: ${person}`);
+// }
+// // Arrow
+// const sayHello = (person) => console.log(`Arrow says Hi: ${person}`);
+// //
+// sayHi(); // Undefined --> Una vez agregado el Default imprimirá el Default hasta que algun parametro se pase
+// sayHello(peter); // Peter
+//
+// Que podemos hacer por el undefined? DEFAULT PARAMETERS
+// function (person = "Default") --> Pasará default hasta que elijamos un parameter
+// Nos permite pasar algo por default en caso de que nadie meta un Parameter
+//
+// Gotcha --> ARROW Functions no pueden ser invokadas antes de ser declaradas, a diferencia de las Functions Regulares!
+
+// --------------------------> ARRAY DESTRUCTURING <----------------------------
 //
 //
 //
 //
 
-// -------------------------->
+// -------------------------->   <----------------------------
 //
 //
 //
 //
 
-// -------------------------->
+// -------------------------->   <----------------------------
 //
 //
 //
 //
 
-// -------------------------->
+// -------------------------->   <----------------------------
 //
 //
 //
 //
 
-// -------------------------->
-//
-//
-//
-//
-
-// -------------------------->
-//
-//
-//
-//
-
-// -------------------------->
-//
-//
-//
-//
-
-// -------------------------->
-//
-//
-//
-//
-
-// -------------------------->
+// -------------------------->   <----------------------------
 //
 //
 //
