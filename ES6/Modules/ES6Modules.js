9a43412bdc4a233299a1d0500fc321b7354d5125
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-26 12:24:09 +0200

    Named Export

--- ES6/Modules/ES6Modules.js
+++ ES6/Modules/ES6Modules.js
@@ -4,10 +4,32 @@
 // We don't have to navigate one GIANT file of JS.
 // React uses the SAME! SYSTEM!!!
 
-// -----------------------> <--------------------------
-//
-//
-//
+// -----------------------> Setup <--------------------------
+// Ejercicio Simple: Tenemos unos Array en js, cuando click shows the array in the div container
+// Haremos without Modules:
+const people = [
+  { name: 'Tom', job: 'Dev' },
+  { name: 'Katy', job: 'Design' },
+  { name: 'Rafa', job: 'Boss' },
+];
+const container = document.querySelector('.container');
+const btn = document.querySelector('.btn');
+// Funcion - Nuevo Array = map de people. Imprimiendo cada Person -
+const showPeople = () => {
+  const newPeople = people
+    .map((person) => {
+      // console.log(person); // Vemos la lista de Person, cada una por separada
+      const { name, job } = person; //Pedimos las Key para cada Person
+      return `<p>${name} <strong>${job} </strong></p>`;
+    })
+    .join(''); // Sacamos la separacion Default = ","
+  //   console.log(newPeople); // Aqui vemos cada item  listos para ser agregados en HTML
+  container.innerHTML = newPeople; // Aqui vemos la lista reflejada en DIV CONTAINER
+};
+// Al Click, Invoke Function
+btn.addEventListener('click', () => {
+  showPeople();
+});
 
 // -----------------------> <--------------------------
 //
