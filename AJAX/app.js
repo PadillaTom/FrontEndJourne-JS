3e7966a9c5ebf667a9535b90bba12d068a8fe24f
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-28 08:56:49 +0200

    AJAX Simple Text

---
+++ AJAX/app.js
@@ -0,0 +1,9 @@
+// ::::::::::::: AJAX :::::::::::::::::
+// AJAX --> Asynchronous Javascript and XML
+// It is an HTTP Request.
+// Communication between Client and Server
+// Instead of hard Coding our websites we will use APIs
+// API = Application Programing Interface --> Rules on HOW to communicate the server so he can give the Data we need.
+//
+// ::::::: Comenzamos con LOCAL STORAGE
+//
